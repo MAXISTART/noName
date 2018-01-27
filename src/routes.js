0bf7dbc624ab50ee869e858f849e31d90411e7c4
zhiyuan3458 <1531026226@qq.com>
2018-01-27 11:05:05 +0800

    warehouse仓库

---
+++ src/routes.js
@@ -0,0 +1,76 @@
+import Login from './views/Login.vue'
+import NotFound from './views/404.vue'
+import Home from './views/Home.vue'
+import Main from './views/Main.vue'
+import buyThingApply from './views/nav1/buyThingApply';
+import Table from './views/nav1/Table.vue'
+// import Form from './views/nav1/Form.vue'
+import user from './views/nav1/user.vue'
+import Page4 from './views/nav2/Page4.vue'
+import Page5 from './views/nav2/Page5.vue'
+import Page6 from './views/nav3/Page6.vue'
+import echarts from './views/charts/echarts.vue'
+
+let routes = [
+    {
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: NotFound,
+        name: '',
+        hidden: true
+    },
+    //{ path: '/main', component: Main },
+    {
+        path: '/',
+        component: Home,
+        name: '办公用品库',
+        iconCls: 'el-icon-message',//图标样式class
+        children: [
+            { path: '/main', component: Main, name: '办公用品采购（暂时不用）', hidden: true },
+            { path: '/buyThingApply', component: buyThingApply, name: '采购申请表' },
+            { path: '/table', component: Table, name: '办公用品申领表' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '库存',
+        iconCls: 'fa fa-id-card-o',
+        children: [
+            { path: '/page4', component: Page4, name: '入库报表' },
+            { path: '/page5', component: Page5, name: '库存表' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-address-card',
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/page6', component: Page6, name: '申领记录' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: 'Charts',
+        iconCls: 'fa fa-bar-chart',
+        children: [
+            { path: '/echarts', component: echarts, name: 'echarts' }
+        ]
+    },
+    {
+        path: '*',
+        hidden: true,
+        redirect: { path: '/404' }
+    }
+];
+
+export default routes;
---
+++ src/views/nav1/Form.vue
@@ -0,0 +1,205 @@
+<template>
+	<el-form status-icon ref="addNewThingForm" :model="form" :rules="rules" label-width="80px" style="margin:20px;width:60%;min-width:600px;">
+		<el-form-item label="入库时间" prop="enterDate">
+			<el-date-picker v-model="form.enterDate" type="date" placeholder="请选择入库日期">
+			</el-date-picker>
+		</el-form-item>
+		<el-form-item label="负责人" prop="warehouseManagerName">
+			<el-select v-model="warehouseManagerDefault" placeholder="请选择负责人">
+				<el-option v-for="item in warehouseManagers" :label="item.warehouseManagerName" :key="item.id" :value="item.id"></el-option>
+			</el-select>
+		</el-form-item>
+		<el-form-item label="名称" prop="thingName">
+			<el-input v-model="form.thingName" placeholder="请输入物品名称" required></el-input>
+		</el-form-item>
+		<el-form-item label="种类" prop="type">
+            <el-input v-model="form.type" placeholder="请输入种类"></el-input>
+		</el-form-item>
+        <el-form-item label="规格" prop="size">
+            <el-input v-model="form.size" placeholder="请输入规格"></el-input>
+        </el-form-item>
+        <el-form-item label="单位" prop="unit">
+            <el-input v-model="form.unit" placeholder="请输入单位"></el-input>
+        </el-form-item>
+        <el-form-item label="数量" required prop="total">
+            <el-input v-model="form.total" placeholder="请输入数量" @blur="computeTotalPrice"></el-input>
+        </el-form-item>
+        <el-form-item label="单价" required prop="unitPrice">
+            <el-input v-model="form.unitPrice" placeholder="请输入单价" @blur="computeTotalPrice">
+				<template slot="append">元</template>
+			</el-input>
+        </el-form-item>
+        <el-form-item label="总价" prop="totalPrice">
+            <el-input v-model="form.totalPrice" readonly>
+				<template slot="append">元</template>
+			</el-input>
+        </el-form-item>
+        <el-form-item label="来源" prop="resource">
+            <el-input v-model="form.resource" placeholder="请输入来源"></el-input>
+        </el-form-item>
+		<el-form-item>
+			<el-button type="primary" @click.prevent="onSubmit('addNewThingForm')">提交</el-button>
+			<el-button @click="resetForm('addNewThingForm')">重置</el-button>
+		</el-form-item>
+	</el-form>
+</template>
+
+<script>
+    import { getWarehouseManager, getWarehouseThings } from '../../api/api';
+    import util from '../../common/js/util.js';
+    const ERR_OK = 200;
+	export default {
+		data() {
+		    // 验证物品名称是否重复
+			var checkThingName = (rule, value, callback) => {
+			    let things = [];
+			    let hasThing = false;
+			    getWarehouseThings().then(res => {
+                    things = res.data.things;
+                    for(var i in things) {
+                        if(things[i] === value) {
+                            hasThing = true;
+                            break;
+                        }
+                    }
+
+                    if(hasThing === true) {
+                        callback(new Error('该物品已存在！'));
+                    }else {
+                        callback();
+                    }
+				});
+			};
+
+		    // 验证数量
+		    var checkTotal = (rule, value, callback) => {
+		        if(!value) {
+		            return callback(new Error('请输入数量！'));
+				}
+				let  rex = /^[1-9]\d*$/;
+				setTimeout(() => {
+		            if(!rex.test(value)) {
+		                callback("	数量格式不正确！");
+					}else {
+		                callback();
+					}
+				}, 1000);
+			};
+
+			// 验证单价
+		    var checkUnitPrice = (rule, value, callback) => {
+                if(!value) {
+                    return callback(new Error('请输入单价！'));
+                }
+                let  rex = /^[0-9]+(.[0-9]{1,2})?$/;
+                setTimeout(() => {
+                    if(!rex.test(value)) {
+                        callback("单价格式不正确！");
+                    }else {
+                        callback();
+                    }
+                }, 1000);
+			};
+
+			return {
+			    state2: '',
+				/* 仓库负责人名单 */
+                warehouseManagers: [],
+                warehouseManagerDefault: '',
+				form: {
+			        enterDate: '',
+                    personInCharge: '',
+					thingName: '',
+                    type: '',
+					unit: '',
+                    total: '',
+                    totalPrice: '',
+                    unitPrice: '',
+                    resource: ''
+				},
+
+                rules: {
+                    enterDate: [
+                        { type:  'date', required: true, message: "请选择入库日期！", trigger: 'change'}
+					],
+                    warehouseManagerName: [
+                        {required: true, message: '请选择负责人', trigger: 'blur'}
+					],
+					thingName: [
+						{validator: checkThingName, trigger: 'blur'}
+					],
+                    type: [
+						{required: true, message: '请输入类型', trigger: 'blur'}
+					],
+					size: [
+                        {required: true, message: '请输入规格', trigger: 'blur'}
+					],
+                    unit: [
+                        {required: true, message: '请输入单位', trigger: 'blur'},
+                        { max: 1, message: '单位格式只能为1个字', trigger: 'blur' }
+					],
+                    total: [
+                        {validator: checkTotal, trigger: 'blur'}
+					],
+                    unitPrice: [
+                        {validator: checkUnitPrice, trigger: 'blur'}
+					],
+                    resource: [
+                        {required: true, message: '请填写来源信息', trigger: 'blur'}
+					]
+				}
+			};
+		},
+		methods: {
+			onSubmit(formName) {
+				this.$refs[formName].validate((valid) => {
+				    if(valid) {
+
+				        alert('提交成功！');
+					}else {
+				        return false;
+					}
+				});
+			},
+
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            },
+
+            computeTotalPrice() {
+                let rexTotal = /^[0-9]*$/;
+                let rexUnitPrice = /^[0-9]+(.[0-9]{1,2})?$/;
+			    if(this.form.total === '' || this.form.unitPrice === '') {
+                    this.form.totalPrice = '';
+                    return;
+				}
+				if(!rexTotal.test(this.form.total)){
+                    this.form.totalPrice = '';
+                    return;
+				}
+
+				if(!rexUnitPrice.test(this.form.unitPrice)) {
+                    this.form.totalPrice = '';
+                    return;
+				}
+
+				let totalPrice = Math.floor(this.form.unitPrice * this.form.total * 100) / 100;
+				if(!isNaN (totalPrice)) {
+					this.form.totalPrice = totalPrice;
+				}else {
+					this.form.totalPrice = '';
+				}
+			}
+		},
+
+        created() {
+            getWarehouseManager().then(res => {
+                if(res.data.code === ERR_OK) {
+                    this.warehouseManagerDefault = res.data.warehouseManager[0].id;
+                    this.warehouseManagers = res.data.warehouseManager;
+				}
+			});
+        }
+	}
+
+</script>
